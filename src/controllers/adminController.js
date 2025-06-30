75024825697a07f8891fbb846730ae1e58762198
lokeshnandan <lokeshnandan007@gmail.com>
2025-06-30 19:21:28 +0530

    get all users

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -512,7 +512,7 @@ exports.getAllUsers = async (req, res, next) => {
   try {
     const { data, error } = await supabaseServiceRole
       .from('profiles')
-      .select('full_name, phone_number, created_at, email');
+      .select('full_name, phone_number, created_at');
     if (error) return res.status(500).json({ success: false, message: error.message });
     return res.json({ success: true, data });
   } catch (err) {

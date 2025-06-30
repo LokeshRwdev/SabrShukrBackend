7e450fac7ea46913cb89817c8bd16c2bc6033f1d
lokeshnandan <lokeshnandan007@gmail.com>
2025-06-30 16:51:12 +0530

    cors config update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'YOUR_REACT_APP_DEPLOYED_URL'], // Add your React app's deployed URL here
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
